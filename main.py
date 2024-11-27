6b46fbf215e67e921e0f60f9a1b9df759544cc4e
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-27 18:31:59 -0300

    feat: create initial components

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ from streamlit_cropper import st_cropper
 from PIL import Image
 
 
-st.title("Hello World")
+st.title("Image cropper")
+
+image_file = st.sidebar.file_uploader(
+    label="Envie uma imagem", type=["png", "jpg", "jpeg"]
+)
+realtime_update = st.sidebar.checkbox("Atualização em tempo real", value=True)
+box_color = st.sidebar.color_picker(label="Grupo de cores", value="#0000FF")
+aspect_choice = st.sidebar.radio(
+    label="Proporção da tela", options=["1:1", "16:9", "4:3"]
+)
+aspect_dict = {"1:1": (1, 1), "16:9": (16, 9), "4:3": (4, 3)}
+aspect_ratio = aspect_dict[aspect_choice]
