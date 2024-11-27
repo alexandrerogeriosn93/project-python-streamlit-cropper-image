b8a4f1d9479f0ab649670637d343f2d6bdbba0e2
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-27 18:51:14 -0300

    feat: create function to edit and show a cropped image

--- main.py
+++ main.py
@@ -15,3 +15,20 @@ aspect_choice = st.sidebar.radio(
 )
 aspect_dict = {"1:1": (1, 1), "16:9": (16, 9), "4:3": (4, 3)}
 aspect_ratio = aspect_dict[aspect_choice]
+
+if image_file:
+    image = Image.open(image_file)
+
+    if not realtime_update:
+        st.write("Duplo clique para cortar a imagem")
+
+    cropped_image = st_cropper(
+        image,
+        realtime_update=realtime_update,
+        box_color=box_color,
+        aspect_ratio=aspect_ratio,
+    )
+
+    cropped = cropped_image.thumbnail((350, 350))
+
+    st.image(cropped_image)
